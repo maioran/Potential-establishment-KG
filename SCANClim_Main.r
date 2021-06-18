e297e98278d5935ab8d5317d2af2d647b1a8b51e
Andrea Maiorano <andrea.maiorano@efsa.europa.eu>
2021-06-18 12:08:24 +0200

    test push

--- SCANClim_Main.r
+++ SCANClim_Main.r
@@ -25,7 +25,7 @@ library(raster)
 library(sp)
 actual.date <- Sys.Date()
 start.time <- Sys.time()
-report.kg <- FALSE
+report.kg <- TRUE
 
 # set main directories
 source("R_scripts\\PEST.PROFILE.main.directories.r")
