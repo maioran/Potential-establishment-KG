700c9bf8ebfd8e8058d262dcf999de4d076de288
Andrea Maiorano <andrea.maiorano@efsa.europa.eu>
2022-01-27 10:44:17 +0100

    removed warnings from script that creates pest specific directories

--- R_scripts/SCANClim.check.pest.directories.r
+++ R_scripts/SCANClim.check.pest.directories.r
@@ -3,26 +3,27 @@
 # This script create needed folder directory for input/output related to specific organism
 # Note: if the folder already exists it does not overwrite it
 ####################################################################################################
+
 # create pest main folder
-dir.create(file.path(pest.dir, pest.name))
+suppressWarnings(dir.create(file.path(pest.dir, pest.name)))
 # create subfolder Output and Review
-dir.create(file.path(pest.dir, pest.name, "\\Output"))
-dir.create(file.path(pest.dir, pest.name, "\\Review"))
+suppressWarnings(dir.create(file.path(pest.dir, pest.name, "\\Output")))
+suppressWarnings(dir.create(file.path(pest.dir, pest.name, "\\Review")))
 # assign variables to output and review directories
-output.dir    <- paste0(pest.dir, pest.name, "\\Output\\")
-review.dir    <- paste0(pest.dir, pest.name, "\\Review\\")
+suppressWarnings(output.dir    <- paste0(pest.dir, pest.name, "\\Output\\"))
+suppressWarnings(review.dir    <- paste0(pest.dir, pest.name, "\\Review\\"))
 # create subfolder Distribution
-dir.create(file.path(paste0(output.dir, "Distribution")))
+suppressWarnings(dir.create(file.path(paste0(output.dir, "Distribution"))))
 # create subfolder Koppen-Geiger
-dir.create(file.path(paste0(output.dir, "Koppen-Geiger")))
+suppressWarnings(dir.create(file.path(paste0(output.dir, "Koppen-Geiger"))))
 # create subfolder including GIS layers
-dir.create(file.path(paste0(output.dir, "GIS")))
+suppressWarnings(dir.create(file.path(paste0(output.dir, "GIS"))))
 # create subfolders Climates and Distribution in Review folder
-dir.create(file.path(paste0(review.dir, "REVIEW.Climates")))
-dir.create(file.path(paste0(review.dir, "REVIEW.Distribution")))
+suppressWarnings(dir.create(file.path(paste0(review.dir, "REVIEW.Climates"))))
+suppressWarnings(dir.create(file.path(paste0(review.dir, "REVIEW.Distribution"))))
 # check if warning file including admin names to check already exists and remove it (if needed it will be built again)
 if(file.exists(paste0(output.dir, "\\Admin.names.to.check.csv")))
 {
   file.remove(paste0(output.dir, "\\Admin.names.to.check.csv"))
 }
-  
+
